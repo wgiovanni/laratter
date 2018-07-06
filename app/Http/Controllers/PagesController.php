e6ad3a511a5197834fddcce56c433501e83625b4
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-07-06 00:41:33 -0400

    Primer commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home()
+    {
+    	$links = [
+			'https://platzi.com/laravel' => 'Curso de Laravel',
+			'https://laravel.com' => 'Página de Laravel'
+		];
+
+    	return view('welcome', [
+    	//'teacher' => 'Guido Contreras Woda',
+    	'links' => $links,
+
+    	]);
+    }
+
+    public function about()
+    {
+    	return view('about');
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="title m-b-md">
+    Acerca de nosotros!
+</div>
+@endsection
+
+@section('title')
+Acerca de nosotros | Laratter
+@endsection
