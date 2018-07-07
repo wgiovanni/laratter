80245ddd770450de6007f86e5fc559b020c37352
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-07-06 20:01:50 -0400

    Agregar Bootstrap al proyecto

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,20 +8,32 @@ class PagesController extends Controller
 {
     public function home()
     {
-    	$links = [
-			'https://platzi.com/laravel' => 'Curso de Laravel',
-			'https://laravel.com' => 'Página de Laravel'
-		];
+    	$messages = [
+            [
+                'id' => 1,
+                'content' => 'Este es mi primer mensaje!',
+                'image' => 'http://lorempixel.com/600/338?1',
+            ],
+            [
+                'id' => 2,
+                'content' => 'Este es mi segundo mensaje!',
+                'image' => 'http://lorempixel.com/600/338?2',
+            ], 
+            [
+                'id' => 3,
+                'content' => 'Este es mi tercer mensaje!',
+                'image' => 'http://lorempixel.com/600/338?3',
+            ],  
+            [
+                'id' => 4,
+                'content' => 'El ultimo mensaje!',
+                'image' => 'http://lorempixel.com/600/338?4',
+            ],            
+        ];
 
     	return view('welcome', [
-    	//'teacher' => 'Guido Contreras Woda',
-    	'links' => $links,
-
+            //'messages' => $messages,
+            'messages' => [],
     	]);
-    }
-
-    public function about()
-    {
-    	return view('about');
     }
 }
