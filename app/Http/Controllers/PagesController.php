c5411db8c16f140666bd4e55e7d1b81189a53753
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-07-13 21:39:41 -0400

    Paginación con Eloquent e implementación de paginación con Bootstrap 4

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function home()
     {
-        $messages = Message::all();
+        $messages = Message::paginate(10); //por defecto son 15
 
         //dd($messages);
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,5 +36,11 @@
 	@empty
 		<p>No hay mensajes destacados</p>
 	@endforelse
+
+	@if(count($messages))
+		<div class="mt-2 mx-auto"> 
+			{{ $messages->links() }}
+		</div>
+	@endif
 </div>
 @endsection
