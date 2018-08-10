e89cc8f949690948537ab829ae39659bbdcb18ca
Wilkel Giovanni <wilkelgiovanni@gmail.com>
2018-08-10 09:31:14 -0400

    Implementación de Unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,13 +25,34 @@ class UsersController extends Controller
     	return redirect("/$username")->withSuccess('Usuario seguido!');
     }
 
+    public function unfollow($username, Request $request)
+    {
+        $user = $this->findByUserName($username);
+
+        $me = $request->user();
+        $me->follows()->detach($user);
+
+        return redirect("/$username")->withSuccess('Usuario no seguido!');
+    }
+
     public function follows($username)
     {
     	$user = $this->findByUserName($username);
 
     	return view('users.follows', [
-    		'user' => $user
+    		'user' => $user,
+            'follows' => $user->follows,
     	]);
+    }
+
+    public function followers($username)
+    {
+        $user = $this->findByUserName($username);
+
+        return view('users.follows', [
+            'user' => $user,
+            'follows' => $user->followers,
+        ]);
     }
 
     private function findByUserName($username)
